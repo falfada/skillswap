6ecd998a0fc6c2c7c2a6279af4ce35ac78b97a03
ANDRESGOM77 <141623667+ANDRESGOM77@users.noreply.github.com>
2024-08-07 21:54:21 +1000

    addig components of Match Sidebar and UserHomepage

---
+++ Client/src/Components/Sidebar/SideBar.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { Link } from "react-router-dom";
+import { FaHome } from "react-icons/fa";
+import { IoChatboxEllipsesOutline } from "react-icons/io5";
+import { CiCalendar } from "react-icons/ci";
+import { CiUser } from "react-icons/ci";
+import { CiSettings } from "react-icons/ci";
+
+function SideBar() {
+  const menus = [
+    { name: "Home", icon: FaHome, link: "/" },
+    { name: "Match", icon: IoChatboxEllipsesOutline, link: "/Match" },
+    { name: "Calendar", icon: CiCalendar, link: "/Calendar" },
+    { name: "User", icon: CiUser, link: "/User" },
+    { name: "Settings", icon: CiSettings, link: "/Settings" },
+  ];
+  const [open, setOpen] = useState(true);
+  return (
+    <section className="flex gap-6">
+      <div
+        className={`bg-[#0e0e0e] min-h-screen ${
+          open ? "w-72" : "w-16"
+        } duration-500 text-gray-100 px-4`}
+      >
+        <div className="py-3 flex justify-end">
+          <GiHamburgerMenu
+            size={26}
+            className="cursor-pointer"
+            onClick={() => setOpen(!open)}
+          />
+        </div>
+        <div className="mt-4 flex flex-col gap-4 relative">
+          {menus.map((menu, index) => (
+            <Link
+              to={menu.link}
+              key={index}
+              className="flex items-center text-sm gap3.6
+            font-medium p-2 hover:bg-gray-800 rounded-md"
+            >
+              <div>{React.createElement(menu.icon, { size: "20" })}</div>
+              <h2
+                className={`whitespace-pre duration-500 ${
+                  !open && "opacity-0 translate-x-28 overflow-hidden"
+                }`}
+              >
+                {menu.name}
+              </h2>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default SideBar;
--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -2,10 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import UserHomepage from './Components/UserHomepage/UserHomepage.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <UserHomepage />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
-hello
+  <RouterProvider router={router} />
+);
