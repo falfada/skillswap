0729e503ea52c03e2118b2846e7428c85da58917
Daniela Leal <falfada@gmail.com>
2024-08-12 06:07:06 -0500

    Setting up mutations and message model

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,76 +1,100 @@
-const { User, Skill } = require("../models");
+const { User, Skill, Message } = require("../models");
+const { signToken, AuthenticationError} = require('../utils/auth');
 
-// TODO: Add the JWT Authentication and GraphQL Error
-const { GraphQLError } = require("graphql");
-const errorMessage = (message, codeMessage) => {
-  return new GraphQLError(message, { extensions: { code: codeMessage } });
-};
 
 const resolvers = {
   Query: {
     me: async (parent, { userId }, context) => {
-      // TODO: Validate
-      // if(context.user){
-      //   return User.findOne({ _id: userId });
-      // }
-      // throw errorMessage("You must be logged in", "UNAUTHENTICATED");
-      return User.findOne({ _id: userId });
+      if(context.user){
+        return User.findOne({ _id: userId });
+      }
+      throw AuthenticationError;
     },
     getSkills: async () => {
       return Skill.find();
     },
     skillMatch: async (parent, { oferredId, learnerId }, context) => {
-      const offerer = await User.findById(oferredId);
-      const learner = await User.findById(learner);
-
-      if (!oferrer || !learner) {
-        throw errorMessage("Oferrer or learner not found", "NOT_FOUND");
+      if(context.user){
+        const offerer = await User.findById(oferredId);
+        const learner = await User.findById(learnerId);
+  
+        if (!offerer || !learner) {
+          throw AuthenticationError;
+        }
+        return User.find({
+          _id: { $ne: oferredId },
+          skilss: { $in: offerer.skills },
+        });  
       }
-      return User.find({
-        _id: { $ne: oferredId },
-        skilss: { $in: oferrer.skills },
-      });
+      throw AuthenticationError
+      
     },
+    getMessages: async(parent, {userId}, context) => {
+      if(context.user){
+        return Message.find({$or: [{sender: userId}, {receiver: userId}]}).populate('sender receiver');
+      }
+      throw AuthenticationError;
+    }
   },
 
   Mutation: {
-    addUser: async (parent, { name, email, password }) => {
-      const user = await User.create({ name, email, password });
-      const token = "placeholder";
-      return { token, user };
-    },
-    login: async (parent, { email, password }) => {
+    login: async (parent, { email, password }, context) => {
       const user = await User.findOne({ email });
       if (!user) {
-        throw errorMessage("User not found", "NOT_FOUND");
+        throw AuthenticationError;
       }
       const verifyPassword = await User.verifyPassword(password);
       if (!verifyPassword) {
-        throw errorMessage("Password incorrect", "UNAUTHENTICATED");
+        throw AuthenticationError;
       }
 
-      // TODO: create JWT token
-      const token = "placeholder";
+      const token = signToken(user);
       return { token, user };
     },
+    addUser: async (parent, { name, email, password }) => {
+      const user = await User.create({ name, email, password });
+      if (!user) {
+        throw AuthenticationError;
+      }
+      const token = signToken(user);
+      return { token, user };
+    },
+    updateUser: async(parent, {name, email, password}, context) => {
+      if(context.user){
+        return User.findOneAndUpdate({_id: context.user._id}, {name, email, password}, {new: true});
+      }
+      throw AuthenticationError;
+    },
     addSkill: async (parent, { userId, skill }, context) => {
-      // if(context.user)
-      return User.findOneAndUpdate(
-        { _id: userId },
-        { $addToSet: { skills: skill } },
-        { new: true, runValidators: true }
-      );
-      // else throw errorMessage("You are not logged in", "UNAUTHENTICATED");
+      if(context.user){
+        return User.findOneAndUpdate(
+          { _id: userId },
+          { $addToSet: { skills: skill } },
+          { new: true, runValidators: true }
+        );
+      }
+      throw AuthenticationError;
     },
-    removeSkill: async (parent, { userId, skill }, context) => {
-      // if(context.user)
-      return User.findOneAndUpdate(
-        { _id: userId },
-        { $pull: { skills: skill } },
-        { new: true }
-      );
-      // else throw errorMessage("You are not logged in", "UNAUTHENTICATED");
+    removeSkill: async (parent, { userId, skillId }, context) => {
+      if(context.user){
+        return User.findOneAndUpdate(
+          { _id: userId },
+          { $pull: { skills: {_id: skillId }}},
+          { new: true }
+        );
+      }
+      throw AuthenticationError;
     },
+    sendMessage: async(parent, {receiverId, content}, context) => {
+      if(context.user){
+        const message = Message.create({sender: context.user._id, receiver: receiverId, content});
+        return await message.populate('sender receiver').execPopulate();
+      }
+      throw AuthenticationError;
+    },
+    addCalendarEvent: async() => {
+      
+    }
   },
 };
 
