37b32bbe12f9ee836a17e916c50c2a8fa6433a86
Daniela Leal <falfada@gmail.com>
2024-08-13 04:54:18 -0500

    Fixing server bugs

--- server/server.js
+++ server/server.js
@@ -7,8 +7,7 @@ const cookieParser = require("cookie-parser");
 const { expressMiddleware } = require('@apollo/server/express4'); // Import expressMiddleware
 const {authMiddleware} = require('./utils/auth');
 
-// const setupLoginRoute = require("./routes/login");
-// const setupAddRoute = require("./routes/add");
+
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
@@ -31,23 +30,6 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
-app.get("/welcome", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/welcome.html"));
-});
-
-app.post('/login', (req, res) => {
-  //authenticate user
-  const username = req.body.username
-  const user = { name: username}
-
-
-  jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
-})
-
-// Set up additional routes
-// setupLoginRoute(app);
-// setupAddRoute(app);
-
 const startApolloServer = async () => {
   await server.start();
 
@@ -64,7 +46,6 @@ const startApolloServer = async () => {
         res.sendFile(path.join(__dirname, '../client/dist/index.html'));
     })
 
-    
 
     db.once('open', () => {
         app.listen(PORT, () => {
@@ -73,95 +54,7 @@ const startApolloServer = async () => {
         });
     })
 
-  // Database Connection and Server Start
-  db.once('open', () => {
-      app.listen(PORT, () => {
-          console.log(`API server running on PORT ${PORT}`);
-          console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
-      });
-  });
 }
 
 startApolloServer();
-
-
-
-
-
-
-
-
-
-
-
-// const express = require('express');
-// const {ApolloServer} = require('@apollo/server');
-// const cors = require('cors');
-
-// const app = express();
-// app.use(cors());
-// app.use(express.json());
-
-// const {expressMiddleware} = require('@apollo/server/express4');
-// const path = require('path');
-// // jwt cookie-parser
-// const cookieParser = require("cookie-parser");
-// // jwt connect login and add routes
-// const setupLoginRoute = require("./routes/login");
-// const setupAddRoute = require("./routes/add");
-
-// const {typeDefs, resolvers} = require('./schemas');
-// const db = require('./config/connection');
-
-// const PORT = 3001;
-// const server = new ApolloServer({typeDefs, resolvers,});
-
-// // parses incoming req URL-encoded payloads
-// // parses cookies attached to the client request object, populating middleware req.cookies with object keyed cookie names
-// // connects to html files - names to be changed
-
-// app.use(
-//     express.urlencoded({
-//       extended: true,
-//     })
-//   );
-//   app.use(cookieParser());
-  
-//   app.get("/", (req, res) => {
-//     res.sendFile(path.join(__dirname, "../public/index.html"));
-//   });
-  
-//   app.get("/welcome", (req, res) => {
-//     res.sendFile(path.join(__dirname, "../public/welcome.html"));
-//   });
-  
-//   setupLoginRoute(app);
-//   setupAddRoute(app);
-
-
-
-// const startApolloServer = async () => {
-//     await server.start();
-
-//     app.use(express.urlencoded({extended: false}));
-//     app.use(express.json());
-
-//     app.use(express.static(path.join(__dirname, '../client/dist')));
-
-//     app.get('*', (req, res) => {
-//         res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-//     })
-
-//     app.use('/graphql', expressMiddleware(server));
-
-//     db.once('open', () => {
-//         app.listen(PORT, () => {
-//             console.log(`API server running on pORT ${PORT}`);
-//             console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
-//         });
-//     })
-
-// }
-
-// startApolloServer();
 
