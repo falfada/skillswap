091fe2e86dcb46bd5dde8ed65d127607c0ff2232
SoniiPP <sonypoudel30@gmail.com>
2024-08-14 16:59:23 +1000

    updating payment system

--- Client/src/Components/Nav.jsx
+++ Client/src/Components/Nav.jsx
@@ -8,6 +8,8 @@ const Nav = () => {
       <div className="space-x-4">
         <Link to="/signup" className="text-gray-600 hover:text-gray-900">Sign up</Link>
         <Link to="/login" className="text-gray-600 hover:text-gray-900">Login</Link>
+        <Link to="/about" className="text-gray-600 hover:text-gray-900">About us</Link>
+
       </div>
     </nav>
   );
